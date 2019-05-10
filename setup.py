3b6a3d3edc60f8a8de3f964275ec92373fcad397
mattsb42 <matt.s.b.42@gmail.com>
2019-05-09 17:39:13 -0700

    no dependencies needed

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@
 import io
 import os
 import re
+import sys
 
-from setuptools import find_packages, setup
+from setuptools import setup
+
+if sys.version_info < (3, 6, 0):
+    print("Python 3.6+ is required")
+    exit(1)
 
 VERSION_RE = re.compile(r"""__version__ = ['"]([0-9.]+)['"]""")
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -20,33 +25,9 @@ def get_version():
     return VERSION_RE.search(init).group(1)
 
 
-def get_requireemnts():
-    """Read the requirements file."""
-    raw_requirements = read("requirements.txt")
-    requirements = []
-    dependencies = []
-
-    for req in raw_requirements.splitlines():
-        req = req.strip()
-        if not req:
-            continue
-        elif req.startswith("#"):
-            continue
-        elif "+" in req:
-            dependencies.append(req)
-        else:
-            requirements.append(req)
-
-    return requirements, dependencies
-
-
-install_requires, dependency_links = get_requireemnts()
-
 setup(
     name="pipipxx",
     version=get_version(),
-    packages=find_packages("src"),
-    package_dir={"": "src"},
     url="https://github.com/mattsb42/pipipxx",
     author="Matt Bullock",
     author_email="m@ttsb42.com",
@@ -56,8 +37,6 @@ setup(
     keywords="pipipxx pipipxx",
     data_files=["README.rst", "CHANGELOG.rst", "LICENSE", "requirements.txt"],
     licence="Apache 2.0",
-    install_requires=install_requires,
-    dependency_links=dependency_links,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
