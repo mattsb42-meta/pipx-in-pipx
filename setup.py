4c755ca01d3c93b8de4173b4a92b8469910e10a2
Matt Bullock <matt.s.b.42@gmail.com>
2019-10-19 11:46:36 +0000

    advertise support for CPython 3.8

--- setup.py
+++ setup.py
@@ -93,6 +93,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
     cmdclass=dict(install=BootstrapInstall),
